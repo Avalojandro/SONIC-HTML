ef318037674fdbec3a03876652a8738a136632ea
avalojandro <avalojandro@gmail.com>
2020-05-22 19:44:51 -0600

    Sonic HTML

--- script2.js
+++ script2.js
@@ -55,6 +55,11 @@ let ring49 = document.querySelector('.ring49');
 let ring50 = document.querySelector('.ring50');
 
 let eggman = document.querySelector('.eggman');
+let spikes = document.querySelector('.spikes');
+let spikes2 = document.querySelector('.spikes2');
+let spikes3 = document.querySelector('.spikes3');
+let spikes4 = document.querySelector('.spikes4');
+let spikes5 = document.querySelector('.spikes5');
 
 
 var song = new Audio('./assests/greenHill.mp3');
@@ -72,6 +77,10 @@ window.addEventListener('load', () => {
     sonic.style.position = 'relative';
     sonic.style.left = "120px";
     sonic.style.top = "95px";
+
+    eggman.style.position = 'relative';
+    eggman.style.left = "925px"; //+50
+    eggman.style.top = "-2130px";  //+40      
 
     ring.style.position = 'relative';
     ring.style.left = "730px";
@@ -137,17 +146,162 @@ window.addEventListener('load', () => {
     ring16.style.left = "2580px"; //+50
     ring16.style.top = "-565px";  //+40
 
-    ring17.style.position = 'relative'; //UP
-    ring17.style.left = "2580px"; //+50
-    ring17.style.top = "-585px";  //+40
+    ring17.style.position = 'relative'; 
+    ring17.style.left = "2630px"; //+50
+    ring17.style.top = "-605px";  //+40
 
-    ring18.style.position = 'relative'; //UP
-    ring18.style.left = "2580px"; //+50
-    ring18.style.top = "-585px";  //+40
+    ring18.style.position = 'relative'; 
+    ring18.style.left = "2680px"; //+50
+    ring18.style.top = "-645px";  //+40
 
-    eggman.style.position = 'relative';
-    eggman.style.left = "1060px"; //+50
-    eggman.style.top = "-2040px";  //+40      
+    ring19.style.position = 'relative'; 
+    ring19.style.left = "2730px"; //+50
+    ring19.style.top = "-685px";  //+40
+
+    ring20.style.position = 'relative'; //UP
+    ring20.style.left = "3130px"; //+50
+    ring20.style.top = "-810px";  //+40
+
+    ring21.style.position = 'relative'; //UP
+    ring21.style.left = "3180px"; //+50
+    ring21.style.top = "-850px";  //+40
+
+    spikes.style.position = 'relative'; //UP
+    spikes.style.left = "3250px"; //+50
+    spikes.style.top = "-925px";  //+40
+
+    ring22.style.position = 'relative'; //UP
+    ring22.style.left = "3350px"; //+50
+    ring22.style.top = "-980px";  //+40
+
+    ring23.style.position = 'relative'; //UP
+    ring23.style.left = "3400px"; //+50
+    ring23.style.top = "-1020px";  //+40
+
+    ring24.style.position = 'relative'; 
+    ring24.style.left = "3125px"; //+50
+    ring24.style.top = "-980px";  //+40
+
+    ring25.style.position = 'relative'; 
+    ring25.style.left = "3175px"; //+50
+    ring25.style.top = "-1020px";  //+40
+
+    ring26.style.position = 'relative'; 
+    ring26.style.left = "3225px"; //+50
+    ring26.style.top = "-1060px";  //+40
+
+    ring27.style.position = 'relative'; 
+    ring27.style.left = "3275px"; //+50
+    ring27.style.top = "-1100px";  //+40
+
+    ring28.style.position = 'relative'; 
+    ring28.style.left = "3325px"; //+50
+    ring28.style.top = "-1140px";  //+40
+
+    ring29.style.position = 'relative'; 
+    ring29.style.left = "3850px"; //+50
+    ring29.style.top = "-1180px";  //+40
+
+    ring30.style.position = 'relative'; 
+    ring30.style.left = "3900px"; //+50
+    ring30.style.top = "-1220px";  //+40
+
+    ring31.style.position = 'relative'; 
+    ring31.style.left = "3950px"; //+50
+    ring31.style.top = "-1260px";  //+40
+
+    ring32.style.position = 'relative'; 
+    ring32.style.left = "4000px"; //+50
+    ring32.style.top = "-1300px";  //+40
+
+    ring33.style.position = 'relative'; 
+    ring33.style.left = "4050px"; //+50
+    ring33.style.top = "-1340px";  //+40
+
+    ring34.style.position = 'relative'; 
+    ring34.style.left = "4100px"; //+50
+    ring34.style.top = "-1380px";  //+40
+
+    ring35.style.position = 'relative'; 
+    ring35.style.left = "4150px"; //+50
+    ring35.style.top = "-1420px";  //+40
+
+    ring36.style.position = 'relative'; 
+    ring36.style.left = "4200px"; //+50
+    ring36.style.top = "-1460px";  //+40
+
+    ring37.style.position = 'relative'; 
+    ring37.style.left = "4250px"; //+50
+    ring37.style.top = "-1500px";  //+40
+
+    ring38.style.position = 'relative'; 
+    ring38.style.left = "4300px"; //+50
+    ring38.style.top = "-1540px";  //+40
+
+    ring39.style.position = 'relative'; 
+    ring39.style.left = "4900px"; //+50
+    ring39.style.top = "-1660px";  //+40
+
+    ring40.style.position = 'relative'; 
+    ring40.style.left = "4970px"; //+50
+    ring40.style.top = "-1700px";  //+40
+
+    ring41.style.position = 'relative'; 
+    ring41.style.left = "5040px"; //+50
+    ring41.style.top = "-1740px";  //+40
+
+    ring42.style.position = 'relative'; 
+    ring42.style.left = "5040px"; //+50
+    ring42.style.top = "-1700px";  //+40
+
+    ring43.style.position = 'relative'; 
+    ring43.style.left = "5090px"; //+50
+    ring43.style.top = "-1740px";  //+40
+
+    ring44.style.position = 'relative'; 
+    ring44.style.left = "5140px"; //+50
+    ring44.style.top = "-1780px";  //+40
+
+    ring45.style.position = 'relative'; 
+    ring45.style.left = "5190px"; //+50
+    ring45.style.top = "-1820px";  //+40
+
+    ring46.style.position = 'relative'; 
+    ring46.style.left = "5240px"; //+50
+    ring46.style.top = "-1860px";  //+40
+
+    ring47.style.position = 'relative'; 
+    ring47.style.left = "5290px"; //+50
+    ring47.style.top = "-1900px";  //+40
+
+    ring48.style.position = 'relative'; 
+    ring48.style.left = "5340px"; //+50
+    ring48.style.top = "-1940px";  //+40
+
+    ring49.style.position = 'relative'; 
+    ring49.style.left = "5630px"; //+50
+    ring49.style.top = "-2050px";  //+40
+
+    ring50.style.position = 'relative'; 
+    ring50.style.left = "5750px"; //+50
+    ring50.style.top = "-2090px";  //+40
+
+    spikes2.style.position = 'relative'; 
+    spikes2.style.left = "3850px"; //+50
+    spikes2.style.top = "-2315px";  //+40
+
+    spikes3.style.position = 'relative'; 
+    spikes3.style.left = "3930px"; //+50
+    spikes3.style.top = "-2405px";  //+40
+
+    spikes4.style.position = 'relative'; 
+    spikes4.style.left = "4010px"; //+50
+    spikes4.style.top = "-2495px";  //+40
+
+    spikes5.style.position = 'relative'; 
+    spikes5.style.left = "5110px"; //+50
+    spikes5.style.top = "-2585px";  //+40
+
 });
 
 //Funciones de movimiento
@@ -164,6 +318,51 @@ function runLeft(){
     ring9.style.left = parseInt(ring9.style.left) + moveBy + 'px';
     ring10.style.left = parseInt(ring10.style.left) + moveBy + 'px';
     ring11.style.left = parseInt(ring11.style.left) + moveBy + 'px';
+    ring12.style.left = parseInt(ring12.style.left) + moveBy + 'px';
+    ring13.style.left = parseInt(ring13.style.left) + moveBy + 'px';
+    ring14.style.left = parseInt(ring14.style.left) + moveBy + 'px';
+    ring15.style.left = parseInt(ring15.style.left) + moveBy + 'px';
+    ring16.style.left = parseInt(ring16.style.left) + moveBy + 'px';
+    ring17.style.left = parseInt(ring17.style.left) + moveBy + 'px';
+    ring18.style.left = parseInt(ring18.style.left) + moveBy + 'px';
+    ring19.style.left = parseInt(ring19.style.left) + moveBy + 'px';
+    ring20.style.left = parseInt(ring20.style.left) + moveBy + 'px';
+    ring21.style.left = parseInt(ring21.style.left) + moveBy + 'px';
+    ring22.style.left = parseInt(ring22.style.left) + moveBy + 'px';
+    ring23.style.left = parseInt(ring23.style.left) + moveBy + 'px';
+    ring24.style.left = parseInt(ring24.style.left) + moveBy + 'px';
+    ring25.style.left = parseInt(ring25.style.left) + moveBy + 'px';
+    ring26.style.left = parseInt(ring26.style.left) + moveBy + 'px';
+    ring27.style.left = parseInt(ring27.style.left) + moveBy + 'px';
+    ring28.style.left = parseInt(ring28.style.left) + moveBy + 'px';
+    ring29.style.left = parseInt(ring29.style.left) + moveBy + 'px';
+    ring30.style.left = parseInt(ring30.style.left) + moveBy + 'px';
+    ring31.style.left = parseInt(ring31.style.left) + moveBy + 'px';
+    ring32.style.left = parseInt(ring32.style.left) + moveBy + 'px';
+    ring33.style.left = parseInt(ring33.style.left) + moveBy + 'px';
+    ring34.style.left = parseInt(ring34.style.left) + moveBy + 'px';
+    ring35.style.left = parseInt(ring35.style.left) + moveBy + 'px';
+    ring36.style.left = parseInt(ring36.style.left) + moveBy + 'px';
+    ring37.style.left = parseInt(ring37.style.left) + moveBy + 'px';
+    ring38.style.left = parseInt(ring38.style.left) + moveBy + 'px';
+    ring39.style.left = parseInt(ring39.style.left) + moveBy + 'px';
+    ring40.style.left = parseInt(ring40.style.left) + moveBy + 'px';
+    ring41.style.left = parseInt(ring41.style.left) + moveBy + 'px';
+    ring42.style.left = parseInt(ring42.style.left) + moveBy + 'px';
+    ring43.style.left = parseInt(ring43.style.left) + moveBy + 'px';
+    ring44.style.left = parseInt(ring44.style.left) + moveBy + 'px';
+    ring45.style.left = parseInt(ring45.style.left) + moveBy + 'px';
+    ring46.style.left = parseInt(ring46.style.left) + moveBy + 'px';
+    ring47.style.left = parseInt(ring47.style.left) + moveBy + 'px';
+    ring48.style.left = parseInt(ring48.style.left) + moveBy + 'px';
+    ring49.style.left = parseInt(ring49.style.left) + moveBy + 'px';
+    ring50.style.left = parseInt(ring50.style.left) + moveBy + 'px';
+
+    spikes.style.left = parseInt(spikes.style.left) + moveBy + 'px';
+    spikes2.style.left = parseInt(spikes2.style.left) + moveBy + 'px';
+    spikes3.style.left = parseInt(spikes3.style.left) + moveBy + 'px';
+    spikes4.style.left = parseInt(spikes4.style.left) + moveBy + 'px';
+    spikes5.style.left = parseInt(spikes5.style.left) + moveBy + 'px';
 
     eggman.style.left = parseInt(eggman.style.left) + moveBy + 'px';
     x--;
@@ -182,6 +381,51 @@ function runRight(){
     ring9.style.left = parseInt(ring9.style.left) - moveBy + 'px';
     ring10.style.left = parseInt(ring10.style.left) - moveBy + 'px';
     ring11.style.left = parseInt(ring11.style.left) - moveBy + 'px';
+    ring12.style.left = parseInt(ring12.style.left) - moveBy + 'px';
+    ring13.style.left = parseInt(ring13.style.left) - moveBy + 'px';
+    ring14.style.left = parseInt(ring14.style.left) - moveBy + 'px';
+    ring15.style.left = parseInt(ring15.style.left) - moveBy + 'px';
+    ring16.style.left = parseInt(ring16.style.left) - moveBy + 'px';
+    ring17.style.left = parseInt(ring17.style.left) - moveBy + 'px';
+    ring18.style.left = parseInt(ring18.style.left) - moveBy + 'px';
+    ring19.style.left = parseInt(ring19.style.left) - moveBy + 'px';
+    ring20.style.left = parseInt(ring20.style.left) - moveBy + 'px';
+    ring21.style.left = parseInt(ring21.style.left) - moveBy + 'px';
+    ring22.style.left = parseInt(ring22.style.left) - moveBy + 'px';
+    ring23.style.left = parseInt(ring23.style.left) - moveBy + 'px';
+    ring24.style.left = parseInt(ring24.style.left) - moveBy + 'px';
+    ring25.style.left = parseInt(ring25.style.left) - moveBy + 'px';
+    ring26.style.left = parseInt(ring26.style.left) - moveBy + 'px';
+    ring27.style.left = parseInt(ring27.style.left) - moveBy + 'px';
+    ring28.style.left = parseInt(ring28.style.left) - moveBy + 'px';
+    ring29.style.left = parseInt(ring29.style.left) - moveBy + 'px';
+    ring30.style.left = parseInt(ring30.style.left) - moveBy + 'px';
+    ring31.style.left = parseInt(ring31.style.left) - moveBy + 'px';
+    ring32.style.left = parseInt(ring32.style.left) - moveBy + 'px';
+    ring33.style.left = parseInt(ring33.style.left) - moveBy + 'px';
+    ring34.style.left = parseInt(ring34.style.left) - moveBy + 'px';
+    ring35.style.left = parseInt(ring35.style.left) - moveBy + 'px';
+    ring36.style.left = parseInt(ring36.style.left) - moveBy + 'px';
+    ring37.style.left = parseInt(ring37.style.left) - moveBy + 'px';
+    ring38.style.left = parseInt(ring38.style.left) - moveBy + 'px';
+    ring39.style.left = parseInt(ring39.style.left) - moveBy + 'px';
+    ring40.style.left = parseInt(ring40.style.left) - moveBy + 'px';
+    ring41.style.left = parseInt(ring41.style.left) - moveBy + 'px';
+    ring42.style.left = parseInt(ring42.style.left) - moveBy + 'px';
+    ring43.style.left = parseInt(ring43.style.left) - moveBy + 'px';
+    ring44.style.left = parseInt(ring44.style.left) - moveBy + 'px';
+    ring45.style.left = parseInt(ring45.style.left) - moveBy + 'px';
+    ring46.style.left = parseInt(ring46.style.left) - moveBy + 'px';
+    ring47.style.left = parseInt(ring47.style.left) - moveBy + 'px';
+    ring48.style.left = parseInt(ring48.style.left) - moveBy + 'px';
+    ring49.style.left = parseInt(ring49.style.left) - moveBy + 'px';
+    ring50.style.left = parseInt(ring50.style.left) - moveBy + 'px';
+
+    spikes.style.left = parseInt(spikes.style.left) - moveBy + 'px';
+    spikes2.style.left = parseInt(spikes2.style.left) - moveBy + 'px';
+    spikes3.style.left = parseInt(spikes3.style.left) - moveBy + 'px';
+    spikes4.style.left = parseInt(spikes4.style.left) - moveBy + 'px';
+    spikes5.style.left = parseInt(spikes5.style.left) - moveBy + 'px';
 
     eggman.style.left = parseInt(eggman.style.left) + moveBy + 'px';
     x++;
@@ -232,6 +476,11 @@ window.addEventListener('keydown', (e)=>{
         rings(74, ring8);
         rings(77, ring9);
         rings(80, ring10);
+
+        rings(135, ring16);
+        rings(138, ring17);
+        rings(141, ring18);
+        rings(144, ring19);
         
     }
 
@@ -262,6 +511,22 @@ window.addEventListener('keydown', (e)=>{
             y=1;
             console.log(y);
             ringsY(36, 39, ring11);
+            ringsY(101, 103, ring12);
+            ringsY(104, 106, ring13);
+            ringsY(107, 109, ring14);
+            ringsY(166, 168, ring20);
+            ringsY(169, 171, ring21);
+
+            ringsY(178, 180, ring22);
+            ringsY(181, 183, ring23);
+
+            ringsY(264, 266, ring39);
+            ringsY(268, 270, ring40);
+
+            ringsY(272, 274, ring41);
+
+            ringsY(305, 307, ring49);
+            ringsY(311, 314, ring50);
         }
 
 
