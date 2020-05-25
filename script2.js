4282af60289664edcc35e09022cfff783b4dd512
avalojandro <avalojandro@gmail.com>
2020-05-24 21:45:10 -0600

    sonic HTML

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-//alert("Estas a punto de jugar SONIC HTML Desarrollado por Avalojandro, asegurate de ajustar el zoom de tu navegador para una mejor experiencia, puedes intentar con un 80%")
+alert("Estas a punto de jugar SONIC HTML Desarrollado por Avalojandro, asegurate de ajustar el zoom de tu navegador para una mejor experiencia, puedes intentar con un 80%")
 
 //Llamando a los elementos
 let background = document.querySelector('.background');
@@ -61,9 +61,17 @@ let spikes3 = document.querySelector('.spikes3');
 let spikes4 = document.querySelector('.spikes4');
 let spikes5 = document.querySelector('.spikes5');
 
+let puntaje = document.querySelector('.score');
+let keepRuning = document.querySelector('.keep');
+let eggman2 = document.querySelector('.eggman2');
 
-var song = new Audio('./assests/greenHill.mp3');
-//song.play();
+let win = document.querySelector('.win');
+let morir = document.querySelector('.die');
+
+//Musica de fondo
+var song = new Audio('./audio/greenHill.mp3');
+song.play();
+
 
 //Velocidad de movimiento
 let moveBy = 18;
@@ -71,6 +79,9 @@ let moveBy = 18;
  
 var x=0; //contador para saber la posicion horizontal de sonic
 var y=0; //contador para saber la posicion vertical de sonic
+
+var score = 0;//puntaje del jugador
+
 
 window.addEventListener('load', () => {
     //Valores iniciales
@@ -139,7 +150,7 @@ window.addEventListener('load', () => {
     ring14.style.top = "-570px";  //+40
     
     ring15.style.position = 'relative'; //UP
-    ring15.style.left = "2580px"; //+50
+    ring15.style.left = "2530px"; //+50
     ring15.style.top = "-525px";  //+40
     
     ring16.style.position = 'relative'; //UP
@@ -302,6 +313,26 @@ window.addEventListener('load', () => {
     spikes5.style.left = "5110px"; //+50
     spikes5.style.top = "-2585px";  //+40
 
+    puntaje.style.position = 'relative'; 
+    puntaje.style.left = "10px"; //+50
+    puntaje.style.top = "-2650px";  //+40
+
+    keepRuning.style.position = 'relative';
+    keepRuning.style.top="-2650px";
+    keepRuning.style.left = "10px"
+
+    eggman2.style.position = 'relative'; 
+    eggman2.style.left = "4000px"; //+50
+    eggman2.style.top = "-2675px";  //+40
+
+    win.style.position = 'relative';
+    win.style.top="-2745px";
+    win.style.left = "220px"
+
+    morir.style.position = 'relative';
+    morir.style.top="-2768px";
+    morir.style.left = "220px"
+
 });
 
 //Funciones de movimiento
@@ -432,30 +463,227 @@ function runRight(){
 
 }
 
-//SALTO
-function jump(){
-    sonic.style.top = parseInt(sonic.style.top) - moveBy + 'px';
- 
+function XrunLeft(){
+    ring.style.left = parseInt(ring.style.left) + 0 + 'px';
+    ring2.style.left = parseInt(ring2.style.left) + 0 + 'px';
+    ring3.style.left = parseInt(ring3.style.left) + 0 + 'px';
+    ring4.style.left = parseInt(ring4.style.left) + 0 + 'px';
+    ring5.style.left = parseInt(ring5.style.left) + 0 + 'px';
+    ring6.style.left = parseInt(ring6.style.left) + 0 + 'px';
+    ring7.style.left = parseInt(ring7.style.left) + 0 + 'px';
+    ring8.style.left = parseInt(ring8.style.left) + 0 + 'px';
+    ring9.style.left = parseInt(ring9.style.left) + 0 + 'px';
+    ring10.style.left = parseInt(ring10.style.left) + 0 + 'px';
+    ring11.style.left = parseInt(ring11.style.left) + 0 + 'px';
+    ring12.style.left = parseInt(ring12.style.left) + 0 + 'px';
+    ring13.style.left = parseInt(ring13.style.left) + 0 + 'px';
+    ring14.style.left = parseInt(ring14.style.left) + 0 + 'px';
+    ring15.style.left = parseInt(ring15.style.left) + 0 + 'px';
+    ring16.style.left = parseInt(ring16.style.left) + 0 + 'px';
+    ring17.style.left = parseInt(ring17.style.left) + 0 + 'px';
+    ring18.style.left = parseInt(ring18.style.left) + 0 + 'px';
+    ring19.style.left = parseInt(ring19.style.left) + 0 + 'px';
+    ring20.style.left = parseInt(ring20.style.left) + 0 + 'px';
+    ring21.style.left = parseInt(ring21.style.left) + 0 + 'px';
+    ring22.style.left = parseInt(ring22.style.left) + 0 + 'px';
+    ring23.style.left = parseInt(ring23.style.left) + 0 + 'px';
+    ring24.style.left = parseInt(ring24.style.left) + 0 + 'px';
+    ring25.style.left = parseInt(ring25.style.left) + 0 + 'px';
+    ring26.style.left = parseInt(ring26.style.left) + 0 + 'px';
+    ring27.style.left = parseInt(ring27.style.left) + 0 + 'px';
+    ring28.style.left = parseInt(ring28.style.left) + 0 + 'px';
+    ring29.style.left = parseInt(ring29.style.left) + 0 + 'px';
+    ring30.style.left = parseInt(ring30.style.left) + 0 + 'px';
+    ring31.style.left = parseInt(ring31.style.left) + 0 + 'px';
+    ring32.style.left = parseInt(ring32.style.left) + 0 + 'px';
+    ring33.style.left = parseInt(ring33.style.left) + 0 + 'px';
+    ring34.style.left = parseInt(ring34.style.left) + 0 + 'px';
+    ring35.style.left = parseInt(ring35.style.left) + 0 + 'px';
+    ring36.style.left = parseInt(ring36.style.left) + 0 + 'px';
+    ring37.style.left = parseInt(ring37.style.left) + 0 + 'px';
+    ring38.style.left = parseInt(ring38.style.left) + 0 + 'px';
+    ring39.style.left = parseInt(ring39.style.left) + 0 + 'px';
+    ring40.style.left = parseInt(ring40.style.left) + 0 + 'px';
+    ring41.style.left = parseInt(ring41.style.left) + 0 + 'px';
+    ring42.style.left = parseInt(ring42.style.left) + 0 + 'px';
+    ring43.style.left = parseInt(ring43.style.left) + 0 + 'px';
+    ring44.style.left = parseInt(ring44.style.left) + 0 + 'px';
+    ring45.style.left = parseInt(ring45.style.left) + 0 + 'px';
+    ring46.style.left = parseInt(ring46.style.left) + 0 + 'px';
+    ring47.style.left = parseInt(ring47.style.left) + 0 + 'px';
+    ring48.style.left = parseInt(ring48.style.left) + 0 + 'px';
+    ring49.style.left = parseInt(ring49.style.left) + 0 + 'px';
+    ring50.style.left = parseInt(ring50.style.left) + 0 + 'px';
+
+    spikes.style.left = parseInt(spikes.style.left) + 0 + 'px';
+    spikes2.style.left = parseInt(spikes2.style.left) + 0 + 'px';
+    spikes3.style.left = parseInt(spikes3.style.left) + 0 + 'px';
+    spikes4.style.left = parseInt(spikes4.style.left) + 0 + 'px';
+    spikes5.style.left = parseInt(spikes5.style.left) + 0 + 'px';
+
+    eggman.style.left = parseInt(eggman.style.left) + 0 + 'px';
+    x--;
+
+}
+//DERECHA  
+function XrunRight(){
+    ring.style.left = parseInt(ring.style.left) - 0 + 'px';
+    ring2.style.left = parseInt(ring2.style.left) - 0 + 'px';
+    ring3.style.left = parseInt(ring3.style.left) - 0 + 'px';
+    ring4.style.left = parseInt(ring4.style.left) - 0 + 'px';
+    ring5.style.left = parseInt(ring5.style.left) - 0 + 'px';
+    ring6.style.left = parseInt(ring6.style.left) - 0 + 'px';
+    ring7.style.left = parseInt(ring7.style.left) - 0 + 'px';
+    ring8.style.left = parseInt(ring8.style.left) - 0 + 'px';
+    ring9.style.left = parseInt(ring9.style.left) - 0 + 'px';
+    ring10.style.left = parseInt(ring10.style.left) - 0 + 'px';
+    ring11.style.left = parseInt(ring11.style.left) - 0 + 'px';
+    ring12.style.left = parseInt(ring12.style.left) - 0 + 'px';
+    ring13.style.left = parseInt(ring13.style.left) - 0 + 'px';
+    ring14.style.left = parseInt(ring14.style.left) - 0 + 'px';
+    ring15.style.left = parseInt(ring15.style.left) - 0 + 'px';
+    ring16.style.left = parseInt(ring16.style.left) - 0 + 'px';
+    ring17.style.left = parseInt(ring17.style.left) - 0 + 'px';
+    ring18.style.left = parseInt(ring18.style.left) - 0 + 'px';
+    ring19.style.left = parseInt(ring19.style.left) - 0 + 'px';
+    ring20.style.left = parseInt(ring20.style.left) - 0 + 'px';
+    ring21.style.left = parseInt(ring21.style.left) - 0 + 'px';
+    ring22.style.left = parseInt(ring22.style.left) - 0 + 'px';
+    ring23.style.left = parseInt(ring23.style.left) - 0 + 'px';
+    ring24.style.left = parseInt(ring24.style.left) - 0 + 'px';
+    ring25.style.left = parseInt(ring25.style.left) - 0 + 'px';
+    ring26.style.left = parseInt(ring26.style.left) - 0 + 'px';
+    ring27.style.left = parseInt(ring27.style.left) - 0 + 'px';
+    ring28.style.left = parseInt(ring28.style.left) - 0 + 'px';
+    ring29.style.left = parseInt(ring29.style.left) - 0 + 'px';
+    ring30.style.left = parseInt(ring30.style.left) - 0 + 'px';
+    ring31.style.left = parseInt(ring31.style.left) - 0 + 'px';
+    ring32.style.left = parseInt(ring32.style.left) - 0 + 'px';
+    ring33.style.left = parseInt(ring33.style.left) - 0 + 'px';
+    ring34.style.left = parseInt(ring34.style.left) - 0 + 'px';
+    ring35.style.left = parseInt(ring35.style.left) - 0 + 'px';
+    ring36.style.left = parseInt(ring36.style.left) - 0 + 'px';
+    ring37.style.left = parseInt(ring37.style.left) - 0 + 'px';
+    ring38.style.left = parseInt(ring38.style.left) - 0 + 'px';
+    ring39.style.left = parseInt(ring39.style.left) - 0 + 'px';
+    ring40.style.left = parseInt(ring40.style.left) - 0 + 'px';
+    ring41.style.left = parseInt(ring41.style.left) - 0 + 'px';
+    ring42.style.left = parseInt(ring42.style.left) - 0 + 'px';
+    ring43.style.left = parseInt(ring43.style.left) - 0 + 'px';
+    ring44.style.left = parseInt(ring44.style.left) - 0 + 'px';
+    ring45.style.left = parseInt(ring45.style.left) - 0 + 'px';
+    ring46.style.left = parseInt(ring46.style.left) - 0 + 'px';
+    ring47.style.left = parseInt(ring47.style.left) - 0 + 'px';
+    ring48.style.left = parseInt(ring48.style.left) - 0 + 'px';
+    ring49.style.left = parseInt(ring49.style.left) - 0 + 'px';
+    ring50.style.left = parseInt(ring50.style.left) - 0 + 'px';
+
+    spikes.style.left = parseInt(spikes.style.left) - 0 + 'px';
+    spikes2.style.left = parseInt(spikes2.style.left) - 0 + 'px';
+    spikes3.style.left = parseInt(spikes3.style.left) - 0 + 'px';
+    spikes4.style.left = parseInt(spikes4.style.left) - 0 + 'px';
+    spikes5.style.left = parseInt(spikes5.style.left) - 0 + 'px';
+
+    eggman.style.left = parseInt(eggman.style.left) + 0 + 'px';
+    x++;
+
+}
+
+function moveDie(){
+    sonic.classList.add("died");
 }
 
 //anillos horizontales
 function rings(value, clase){
     if(x==value){
-        clase.classList.add("blink");}
+        clase.classList.add("blink");
+        var ringSound = new Audio('./audio/ringSound.mp3');
+        ringSound.play();
+        }
 }
 
 //anillos verticales
 function ringsY(v1, v2, clase){
     if(y == 1){
         if(x>=v1 && x<=v2){
-            clase.classList.add("blink");}
+            clase.classList.add("blink");
+            var ringSound = new Audio('./audio/ringSound.mp3');
+            ringSound.play();}
         }
 }
 
+//contador de puntaje
+function scoreR(value){
+    if(x==value){
+        score++;
+        //console.log(score);
+        document.getElementById("puntaje").innerHTML = score;
+        }
+}
+
+function scoreL(value){
+    if(x==value){
+        score--;
+        //console.log(score);
+        }
+}
+
+function scoreY(v1,v2){
+    if(y == 1){
+        if(x>=v1 && x<=v2){
+            score++;
+            //console.log(score);
+            document.getElementById("puntaje").innerHTML = score;
+        }
+            }
+}
+
+var die = 0;
+function lose(p1,p2){
+    if(y == 1){
+        if(x>=p1 && x<=p2){
+            score++;
+            //console.log("you lose");
+            sonic.classList.add("died");
+            die++;
+
+        }
+            }
+}
+
+function ganar(v, clase){
+    if(x==v){
+        clase.classList.add("eggLoses");
+        win.classList.add("mostrar");
+        }
+}
+
+function repetir(v){
+    if(x==v){
+        alert("PRESS F5 TO PLAY AGAIN");
+    }
+}
+
+
+
+//Eggman
+function eggMan(){
+    if(score >= 50){
+        keepRuning.classList.add("keep2");
+    }
+    return;
+}
+
+
 //KEYDOWN
+
 window.addEventListener('keydown', (e)=>{
+
+    var jump = new Audio('./audio/jump.mp3');
+
     if(e.key == "ArrowRight"){
           runRight();
+
         sonic.classList.add("moving");
         sonic.classList.remove("sonic2");
         sonic.classList.add("sonic");
@@ -463,8 +691,51 @@ window.addEventListener('keydown', (e)=>{
         eggman.classList.add("eggRun");
         eggman.classList.remove("eggman");
         y=0;
+        if(die>0){
+            morir.classList.add("mostrar");
+            alert("PRESS F5 TO PLAY AGAIN");
+        }
+        
+        
         console.log(x);
-
+       // eggMan();
+        scoreR(31);
+        scoreR(35);
+        scoreR(38);
+        scoreR(41);
+        scoreR(66);
+        scoreR(69);
+        scoreR(71);
+        scoreR(74);
+        scoreR(77);
+        scoreR(80);
+        scoreR(132);
+        scoreR(135);
+        scoreR(138);
+        scoreR(141);
+        scoreR(144);
+        scoreR(165);
+        scoreR(168);
+        scoreR(171);
+        scoreR(174);
+        scoreR(177);
+        scoreR(205);
+        scoreR(208);
+        scoreR(211);
+        scoreR(214);
+        scoreR(217);
+        scoreR(220);
+        scoreR(223);
+        scoreR(226);
+        scoreR(229);
+        scoreR(232);
+        scoreR(272);
+        scoreR(275);
+        scoreR(278);
+        scoreR(281);
+        scoreR(284);
+        scoreR(287);
+        scoreR(288);
 
         rings(31, ring);
         rings(35, ring2);
@@ -476,61 +747,131 @@ window.addEventListener('keydown', (e)=>{
         rings(74, ring8);
         rings(77, ring9);
         rings(80, ring10);
-
+        rings(132, ring15);
         rings(135, ring16);
         rings(138, ring17);
         rings(141, ring18);
         rings(144, ring19);
-        
+        rings(165, ring24);
+        rings(168, ring25);
+        rings(171, ring26);
+        rings(174, ring27);
+        rings(177, ring28);
+        rings(205, ring29);
+        rings(208, ring30);
+        rings(211, ring31);
+        rings(214, ring32);
+        rings(217, ring33);
+        rings(220, ring34);
+        rings(223, ring35);
+        rings(226, ring36);
+        rings(229, ring37);
+        rings(232, ring38);
+        rings(272, ring42);
+        rings(275, ring43);
+        rings(278, ring44);
+        rings(281, ring45);
+        rings(284, ring46);
+        rings(287, ring47);
+        rings(288, ring48);
+
+        ganar(528, eggman2);
+        repetir(531);
+
+        if(score >= 50){
+            eggman2.style.left = parseInt(eggman2.style.left) - moveBy + 'px';
+        }
     }
 
         if(e.key == "ArrowLeft"){
              runLeft();
              y=0;
-             console.log(x);
+             //console.log(x);
+
             sonic.classList.add("moving2");
             background.classList.add("background2");
             eggman.classList.add("eggRun");
             eggman.classList.remove("eggman");
-            
-            rings(31, ring);
-            rings(35, ring2);
-            rings(38, ring3);
-            rings(41, ring4);
-            rings(44, ring5);
-            rings(47, ring6);
-            rings(49, ring7);
-            rings(52, ring8);
-            rings(55, ring9);
-            rings(58, ring10);
-        
-        }
 
+            if(die>0){
+                morir.classList.add("mostrar");
+                alert("PRESS F5 TO PLAY AGAIN");
+            }
+
+        //eggMan();
+        scoreL(31);
+        scoreL(35);
+        scoreL(38);
+        scoreL(41);
+        scoreL(66);
+        scoreL(69);
+        scoreL(71);
+        scoreL(74);
+        scoreL(77);
+        scoreL(80);
+        scoreL(132);
+        scoreL(135);
+        scoreL(138);
+        scoreL(141);
+        scoreL(144);
+        scoreL(165);
+        scoreL(168);
+        scoreL(171);
+        scoreL(174);
+        scoreL(177);
+        scoreL(205);
+        scoreL(208);
+        scoreL(211);
+        scoreL(214);
+        scoreL(217);
+        scoreL(220);
+        scoreL(223);
+        scoreL(226);
+        scoreL(229);
+        scoreL(232);
+        scoreL(272);
+        scoreL(275);
+        scoreL(278);
+        scoreL(281);
+        scoreL(284);
+        scoreL(287);
+        scoreL(288);
+
+        if(score >= 50){
+            eggman2.style.left = parseInt(eggman2.style.left) + moveBy + 'px';
+            
+        }
+        }
             if(e.key == "ArrowUp"){                
             sonic.classList.add("jumping");
             y=1;
-            console.log(y);
+            
+            jump.play();
+            //console.log(y);
+            if(die>0){
+                morir.classList.add("mostrar");
+                alert("PRESS F5 TO PLAY AGAIN");
+            }
+
             ringsY(36, 39, ring11);
             ringsY(101, 103, ring12);
             ringsY(104, 106, ring13);
             ringsY(107, 109, ring14);
             ringsY(166, 168, ring20);
             ringsY(169, 171, ring21);
-
             ringsY(178, 180, ring22);
             ringsY(181, 183, ring23);
-
             ringsY(264, 266, ring39);
             ringsY(268, 270, ring40);
-
             ringsY(272, 274, ring41);
-
             ringsY(305, 307, ring49);
             ringsY(311, 314, ring50);
+
         }
 
-
 });
+
+
 
 //KEYUP
 window.addEventListener('keyup', (e)=>{
@@ -549,10 +890,36 @@ window.addEventListener('keyup', (e)=>{
             eggman.classList.add("eggman");}
 
             if(e.key == "ArrowUp"){
+                
                 sonic.classList.remove("jumping");
+                
+                if(die > 0){
+                    morir.classList.add("mostrar");
+                }
+
+            scoreY(36, 39);
+            scoreY(101, 103);
+            scoreY(104, 106);
+            scoreY(107, 109);
+            scoreY(166, 168);
+            scoreY(169, 171);
+            scoreY(178, 180);
+            scoreY(181, 183);
+            scoreY(264, 266);
+            scoreY(268, 270);
+            scoreY(272, 274);
+            scoreY(305, 307);
+            scoreY(311, 314);
+
+
+            lose(172,177);
+            lose(205,219);
+            lose(275,280);
+
+            eggMan();
             }
 });
 
-
-
-
+if(die>1){
+    morir.classList.add("mostrar");
+}
